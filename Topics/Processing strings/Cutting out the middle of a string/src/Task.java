5aa80819f6f0fe29ffa928a45847ffdd15522fc8
Taofeek Alao <talao3@gatech.edu>
2021-12-25 22:09:55 +0100

    refactored cutting out the middle

--- Topics/Processing strings/Cutting out the middle of a string/src/Task.java
+++ Topics/Processing strings/Cutting out the middle of a string/src/Task.java
@@ -1,7 +1,33 @@
-// write your answer here 
+import java.util.Scanner;
 
 public class Task {
-  public static void main(String[] args) {
-    // put your code here
-  }
+/*    public static void main(String[] args) {
+        // put your code here
+
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString.length() % 2 != 0) {
+                if (i == inputString.length() / 2) {
+                    continue;
+                }
+                System.out.print(inputString.charAt(i));
+            } else {
+                if (i == inputString.length() / 2 - 1 || i == inputString.length() / 2) {
+                    continue;
+                }
+                System.out.print(inputString.charAt(i));
+            }
+        }
+    }*/
+
+    public static void main(String[] args) {
+        // put your code here
+
+        StringBuilder inputStringBuilder = new StringBuilder(new java.util.Scanner(System.in).nextLine());
+        System.out.println(inputStringBuilder.length() % 2 != 0 ?
+                inputStringBuilder.deleteCharAt(inputStringBuilder.length() / 2) :
+                inputStringBuilder.delete(inputStringBuilder.length() / 2 - 1, inputStringBuilder.length() / 2 + 1));
+    }
 }
